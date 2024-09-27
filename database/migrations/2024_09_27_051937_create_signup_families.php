ab795a2d2c33b598e2ccf0d866a3953412ba65f3
Hkaar <HkaarDev@gmail.com>
2024-09-27 14:30:32 +0800

    Added initial migrations

---
+++ database/migrations/2024_09_27_051937_create_signup_families.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('signup_families', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('family_member_type_id')->constrained('family_member_types')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreignId('signup_id')->constrained('signups')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->string('name');
+            $table->string('phone');
+            $table->foreignId('signup_job_id')->constrained('signup_jobs')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('signup_families');
+    }
+};
