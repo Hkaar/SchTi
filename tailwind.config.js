cd138c77c52ede8b61265d1364694b3d11d31621
Hkaar <HkaarDev@gmail.com>
2024-10-04 17:30:11 +0800

    Added more npm dependecies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
+  mode: 'jit',
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     'node_modules/preline/dist/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      }
+    },
   },
   plugins: [
     require('preline/plugin'),
